ce6691d13b508068c9b1141b30ebe19addde85b4
Mizzuna <29733586+aaldyapriansyah@users.noreply.github.com>
2017-09-17 06:13:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,3 +45,7 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+if "Say " in msg. text: 
+	bctxt = msg.text.replace("Say ","")
+	sendMessage(msg.to,(bctxt))
